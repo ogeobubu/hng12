6ca6e52de322901241888ae16abc7a9f907bf80b
ogeobubu <ogeobubu@gmail.com>
2025-02-01 12:24:44 +0100

    first commit

---
+++ stage0/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/api/info", (req, res) => {
+  const data = {
+    email: "ogeobubu@gmail.com",
+    current_datetime: new Date().toISOString(),
+    github_url: "https://github.com/ogeobubu/hng12",
+  };
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log(`API running at http://localhost:${port}/api/info`);
+});
